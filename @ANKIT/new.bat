19cdb9cb5d7b6f9063560c8dde8d8c93f31c49d0
vikaspandey97 <80974496+vikaspandey97@users.noreply.github.com>
2024-05-09 15:02:56 +0530

    Add files via upload

---
+++ @ANKIT/new.bat
@@ -0,0 +1,10 @@
+@echo off
+start "" "chrome" https://auto.mahindra.com/
+start "" "chrome" https://mahindrauat--aceuat.sandbox.lightning.force.com/lightning/r/Case/500C300000ADmaZIAT/view
+start "" "chrome" http://10.250.217.224/MRC_FAQ/
+start "" "chrome" http://10.250.217.224/MRC_FAQ/Home/link_WYH?link=http%3A%2F%2F10.250.217.224%2Fmrcfaq_links%2FAgent_Reference_and_Tolfree_number_2021_05_14_21_30_59.htm
+start "" "chrome" http://10.250.217.224/MRC_FAQ/Home/link_WYH?link=http%3A%2F%2F10.250.217.224%2Fmrcfaq_links%2FChassis_number_2019_08_10_12_06_21.htm
+start "" "chrome" http://10.250.217.224/MRC_FAQ/Home/link_WYH?link=http%3A%2F%2F10.250.217.224%2Fmrcfaq_links%2FBS6_Maintenance_Schedule_2021_09_30_11_37_17.htm
+start "" "chrome" https://app.cognocart.com/chat/bot/?id=105
+start "" "chrome" http://10.250.217.224/MRC_FAQ/Home/link_WYH?link=http%3A%2F%2F10.250.217.224%2Fmrcfaq_links%2FHot_Connect_Scenarios_Version_2.0_2024_02_27_17_44_31.htm
+start "" "chrome" https://115.113.37.250/DMSContext/dmscommon/LoginAction.action?loginFlag=TXNS
